14bcf8b92b10a4d0644f55df4d85ee6002329fc9
Your Name <aaa@bbb.com>
2018-06-21 17:06:04 +0800

    springboot第一次提交，刚创建完，能启动服务器，没任何内容。

---
+++ src/main/java/com/yang/ModelSpringbootApplication.java
@@ -0,0 +1,12 @@
+package com.yang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModelSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModelSpringbootApplication.class, args);
+	}
+}
